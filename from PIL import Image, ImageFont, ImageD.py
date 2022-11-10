dd2cdccc354185a5343be53ad9ee1e32aad92892
Turan <34771@ma-web.nl>
2022-11-10 15:56:27 +0100

    hoitate

--- from PIL import Image, ImageFont, ImageD.py
+++ from PIL import Image, ImageFont, ImageD.py
@@ -1,6 +1,7 @@
 import PIL
 from PIL import Image, ImageFont, ImageDraw
-
+import pathlib
+import os
 achtergrond = Image.open("meme_background.jpg")
 
 breedte = achtergrond.width
@@ -17,8 +18,6 @@ tekengebied.multiline_text((10,10), tekst, font=lettertype, fill=(0,0,0))
 achtergrond.show()
 
 
-achtergrond.save("meme_met_tekst.jpg")
-
 tekst_breedte, tekst_hoogte = tekengebied.textsize(tekst, font=lettertype) 
 
 print("tekstbreedte=" + str(tekst_breedte) + ", tekst_hoogte=" + str(tekst_hoogte))
@@ -27,3 +26,5 @@ tekst_x = (breedte - tekst_breedte) / 2
 tekst_y = (hoogte - tekst_hoogte) / 2
 
 tekengebied.multiline_text((tekst_x-2, tekst_y-2), tekst, font=lettertype, fill=(255,255,255))
+
+achtergrond.save("meme_background.jpg")
